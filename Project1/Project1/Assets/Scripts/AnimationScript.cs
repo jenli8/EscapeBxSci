58b9620bc4d210572d81b32ab3bcddfcd8c2cc9b
jennykittyli <lij9@bxscience.edu>
2016-05-22 21:07:33 -0400

    Adds third floor and Photon Networking Imports

---
+++ Project1/Project1/Assets/Scripts/AnimationScript.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationScript : MonoBehaviour {
+	
+	public Animation SlimeAnimator; 
+	public GameObject slime; 
+	float timer= 200f; 
+	public int health = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+		//SlimeAnimator = GetComponent<Animation> (); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/*toggleAnimations (); 
+		timer -= 1.0f; 
+		slime.GetComponent<Rigidbody>().AddForce (transform.forward * 2);*/
+	}
+	/*	
+	void toggleAnimations() {
+		if (timer % 99 == 10) {
+			SlimeAnimator.Play ("Attack");
+		} else {
+			SlimeAnimator.Play("Wait");  
+		}
+	}
+*/
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("book")) {
+			health -= 1;
+			SlimeAnimator.Play ("Damage");
+			if (health == 0) {
+				SlimeAnimator.Play ("Dead");
+				Destroy (gameObject);
+			}
+			Destroy (other.gameObject);
+		}
+	}
+}
---
+++ Project1/Project1/Assets/Scripts/InstructionsDisplay.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsDisplay : MonoBehaviour {
+
+	private float timed;
+	// Use this for initialization
+	void Start () {
+		timed = 10; 
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timed -= Time.deltaTime; 
+		if (timed <= 0) {
+			Destroy (gameObject);
+		}
+	}
+	
+}
